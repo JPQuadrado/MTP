489bbba184bdb1eb652c9e01f58df9af0d257f69
JPQuadrado <j.pedrocorrea13>
2017-09-07 19:39:09 -0300

    fim de aula

--- aula20170905/ger1.c
+++ aula20170905/ger1.c
@@ -2,5 +2,15 @@
 
 int main ()
 {
-	int 
+	int X,cont = 0;
+	printf("Ola usuario, digite um numero inteiro \n");
+	scanf("%d", &X);
+	for(int k = 2; k <= X; k++)
+		if(X%k == 0)
+			cont++;
+	if(cont == 1)
+		printf("O numero %d e PRIMO \n", X);
+	else
+		printf("O numero %d e um NAO PRIMO \n",X);
+	return 0;
 }
